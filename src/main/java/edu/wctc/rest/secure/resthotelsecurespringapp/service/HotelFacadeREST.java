135fdc4bd7908b953e092d25fc2742795deaa3d8
Jim Lombardo <james.g.lombardo@gmail.com>
2014-11-20 12:23:57 -0600

    springsecure version. Still not working.

--- src/main/java/edu/wctc/rest/secure/resthotelsecurespringapp/service/HotelFacadeREST.java
+++ src/main/java/edu/wctc/rest/secure/resthotelsecurespringapp/service/HotelFacadeREST.java
@@ -2,7 +2,6 @@ package edu.wctc.rest.secure.resthotelsecurespringapp.service;
 
 import edu.wctc.rest.secure.resthotelsecurespringapp.entity.Hotel;
 import java.util.List;
-import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.ws.rs.Consumes;
@@ -13,12 +12,13 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import org.springframework.stereotype.Repository;
 
 /**
  *
  * @author jlombardo
  */
-@Stateless
+@Repository
 @Path("v1/hotels")
 public class HotelFacadeREST extends AbstractFacade<Hotel> {
     @PersistenceContext(unitName = "edu.wctc.rest.secure_RestHotelSecureSpringApp_war_1.0-SNAPSHOTPU")
